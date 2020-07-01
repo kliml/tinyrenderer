b160d891193e8215bbf4233684e5c63bbb36fbf6
Klim <klim.lestsenko@gmail.com>
2020-07-01 21:21:55 +0300

    remove nalgebra vectors

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,18 +1,20 @@
-use nalgebra::Vector3;
 use tobj::Mesh;
 
+#[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
+pub struct Vec3f {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
 pub struct Triangle {
-    pub v1: Vector3<f32>,
-    pub v2: Vector3<f32>,
-    pub v3: Vector3<f32>,
+    pub vertices: [Vec3f; 3],
 }
 
 impl Clone for Triangle {
     fn clone(&self) -> Triangle {
         Triangle {
-            v1: self.v1,
-            v2: self.v2,
-            v3: self.v3,
+            vertices: self.vertices,
         }
     }
 }
@@ -29,26 +31,25 @@ impl ToSimpleMesh for Mesh {
     fn to_simple_mesh(&self) -> SimpleMesh {
         let mut triangles = vec![
             Triangle {
-                // Repeat this triangle for all faces in polygon
-                v1: Vector3::new(0.0, 0.0, 0.0),
-                v2: Vector3::new(0.0, 0.0, 0.0),
-                v3: Vector3::new(0.0, 0.0, 0.0)
+                vertices: [Vec3f {
+                    x: 0.0,
+                    y: 0.0,
+                    z: 0.0
+                }; 3],
             };
             self.indices.len() / 3
         ];
         for (x, tri) in triangles.iter_mut().enumerate() {
-            tri.v1.x = self.positions[(self.indices[x * 3] * 3) as usize];
-            tri.v1.y = self.positions[(self.indices[x * 3] * 3 + 1) as usize];
-            tri.v1.z = self.positions[(self.indices[x * 3] * 3 + 2) as usize];
-            tri.v2.x = self.positions[(self.indices[x * 3 + 1] * 3) as usize];
-            tri.v2.y = self.positions[(self.indices[x * 3 + 1] * 3 + 1) as usize];
-            tri.v2.z = self.positions[(self.indices[x * 3 + 1] * 3 + 2) as usize];
-            tri.v3.x = self.positions[(self.indices[x * 3 + 2] * 3) as usize];
-            tri.v3.y = self.positions[(self.indices[x * 3 + 2] * 3 + 1) as usize];
-            tri.v3.z = self.positions[(self.indices[x * 3 + 2] * 3 + 2) as usize];
+            tri.vertices[0].x = self.positions[(self.indices[x * 3] * 3) as usize];
+            tri.vertices[0].y = self.positions[(self.indices[x * 3] * 3 + 1) as usize];
+            tri.vertices[0].z = self.positions[(self.indices[x * 3] * 3 + 2) as usize];
+            tri.vertices[1].x = self.positions[(self.indices[x * 3 + 1] * 3) as usize];
+            tri.vertices[1].y = self.positions[(self.indices[x * 3 + 1] * 3 + 1) as usize];
+            tri.vertices[1].z = self.positions[(self.indices[x * 3 + 1] * 3 + 2) as usize];
+            tri.vertices[2].x = self.positions[(self.indices[x * 3 + 2] * 3) as usize];
+            tri.vertices[2].y = self.positions[(self.indices[x * 3 + 2] * 3 + 1) as usize];
+            tri.vertices[2].z = self.positions[(self.indices[x * 3 + 2] * 3 + 2) as usize];
         }
-        SimpleMesh {
-            triangles,
-        }
+        SimpleMesh { triangles }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use image::{ ImageBuffer, Pixel, Rgb };
 use image::imageops::*;
+use image::{ImageBuffer, Pixel, Rgb};
 use num;
 use std::mem;
 use tobj;
 
-fn line(x0: i32, y0: i32, x1: i32, y1: i32, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, color: Rgb<u8>) {
+mod geometry;
+use geometry::ToSimpleMesh;
+use geometry::Vec3f;
+
+fn line(
+    x0: i32,
+    y0: i32,
+    x1: i32,
+    y1: i32,
+    image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
+    color: Rgb<u8>,
+) {
     let (mut x0, mut x1, mut y0, mut y1) = (x0, x1, y0, y1);
     let mut steep = false;
     if num::abs(x0 - x1) < num::abs(y0 - y1) {
@@ -31,56 +42,186 @@ fn line(x0: i32, y0: i32, x1: i32, y1: i32, image: &mut ImageBuffer<Rgb<u8>, Vec
         }
         error += derror;
         if error > dx {
-            y += if y1 > y0 {1} else {-1};
-            error -= dx * 2; 
+            y += if y1 > y0 { 1 } else { -1 };
+            error -= dx * 2;
         }
     }
+}
+
+fn triangle(
+    t0: &Vec3f,
+    t1: &Vec3f,
+    t2: &Vec3f,
+    image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
+    color: Rgb<u8>,
+) {
+    line(
+        t0.x as i32,
+        t0.y as i32,
+        t1.x as i32,
+        t1.y as i32,
+        image,
+        color,
+    );
+    line(
+        t0.x as i32,
+        t0.y as i32,
+        t2.x as i32,
+        t2.y as i32,
+        image,
+        color,
+    );
+    line(
+        t1.x as i32,
+        t1.y as i32,
+        t2.x as i32,
+        t2.y as i32,
+        image,
+        color,
+    );
 }
 
 fn main() {
     let white = Rgb::from_channels(255, 255, 255, 255);
     let red = Rgb::from_channels(255, 0, 0, 255);
+    let green = Rgb::from_channels(0, 255, 0, 255);
 
     let (width, height) = (800, 800);
-    let mut imgbuf: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(width + 1, height + 1);
+    let mut imgbuf: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(width, height + 1);
 
     let obj = tobj::load_obj("obj/african_head.obj", true);
-    
+
     let (models, materials) = obj.unwrap();
 
     println!("Uploading model: {}", models[0].name);
     let mesh = &models[0].mesh;
-    
-    let mut faces = vec![];
-    let mut next_face = 0;
-    for f in 0..mesh.num_face_indices.len() {
-        let end = next_face + mesh.num_face_indices[f] as usize;
-        let face_indices: Vec<_> = mesh.indices[next_face..end].iter().collect();
-        faces.push(face_indices);
-        next_face = end;
-    }
 
-    let mut vertices = vec![];
-    for v in (0..mesh.positions.len()).step_by(3) {
-        let vertice: Vec<_> = mesh.positions[v..v+3].iter().collect();
-        vertices.push(vertice);
-    }
+    // let mut faces = vec![];
+    // let mut next_face = 0;
+    // for f in 0..mesh.num_face_indices.len() {
+    //     let end = next_face + mesh.num_face_indices[f] as usize;
+    //     let face_indices: Vec<_> = mesh.indices[next_face..end].iter().collect();
+    //     faces.push(face_indices);
+    //     next_face = end;
+    // }
 
-    for face in &faces {
-        for i in 0..3 {
-            let vert1 = &vertices[*face[i] as usize];
-            let vert2 = &vertices[*face[(i+1)%3] as usize];
+    // let mut vertices = vec![];
+    // for v in (0..mesh.positions.len()).step_by(3) {
+    //     let vertice: Vec<_> = mesh.positions[v..v+3].iter().collect();
+    //     vertices.push(vertice);
+    // }
 
-            let x0 = ((vert1[0]+1.)*width as f32 *0.5) as i32;
-            let y0 = ((vert1[1]+1.)*height as f32 *0.5) as i32;
-            let x1 = ((vert2[0]+1.)*width as f32 *0.5) as i32;
-            let y1 = ((vert2[1]+1.)*height as f32 *0.5) as i32;
+    // for face in &faces {
+    //     for i in 0..3 {
+    //         let vert1 = &vertices[*face[i] as usize];
+    //         let vert2 = &vertices[*face[(i+1)%3] as usize];
 
-            line(x0, y0, x1, y1, &mut imgbuf, white);
-        }
-    }
+    //         let x0 = ((vert1[0]+1.)*width as f32 *0.5) as i32;
+    //         let y0 = ((vert1[1]+1.)*height as f32 *0.5) as i32;
+    //         let x1 = ((vert2[0]+1.)*width as f32 *0.5) as i32;
+    //         let y1 = ((vert2[1]+1.)*height as f32 *0.5) as i32;
+
+    //         line(x0, y0, x1, y1, &mut imgbuf, white);
+    //     }
+    // }
+
+    let t0 = geometry::Triangle {
+        vertices: [
+            Vec3f {
+                x: 10.0,
+                y: 70.0,
+                z: 0.0,
+            },
+            Vec3f {
+                x: 50.0,
+                y: 160.0,
+                z: 0.0,
+            },
+            Vec3f {
+                x: 70.0,
+                y: 80.0,
+                z: 0.0,
+            },
+        ],
+    };
+    let t1 = geometry::Triangle {
+        vertices: [
+            Vec3f {
+                x: 180.0,
+                y: 50.0,
+                z: 0.0,
+            },
+            Vec3f {
+                x: 150.0,
+                y: 1.0,
+                z: 0.0,
+            },
+            Vec3f {
+                x: 70.0,
+                y: 180.0,
+                z: 0.0,
+            },
+        ],
+    };
+    let t2 = geometry::Triangle {
+        vertices: [
+            Vec3f {
+                x: 180.0,
+                y: 150.0,
+                z: 0.0,
+            },
+            Vec3f {
+                x: 120.0,
+                y: 160.0,
+                z: 0.0,
+            },
+            Vec3f {
+                x: 130.0,
+                y: 180.0,
+                z: 0.0,
+            },
+        ],
+    };
+
+    triangle(
+        &t0.vertices[0],
+        &t0.vertices[1],
+        &t0.vertices[2],
+        &mut imgbuf,
+        red,
+    );
+    triangle(
+        &t1.vertices[0],
+        &t1.vertices[1],
+        &t1.vertices[2],
+        &mut imgbuf,
+        white,
+    );
+    triangle(
+        &t2.vertices[0],
+        &t2.vertices[1],
+        &t2.vertices[2],
+        &mut imgbuf,
+        green,
+    );
+
+    // let object = mesh.to_simple_mesh();
+    // for triangle in object.triangles {
+    //     let points = triangle.raw();
+    //     for i in 0..3 {
+    //         let vert1 = points[i];
+    //         let vert2 = points[(i + 1) % 3];
+
+    //         let x0 = ((vert1[0] +1.)*width as f32 *0.5) as i32;
+    //         let y0 = ((vert1[1] +1.)*height as f32 *0.5) as i32;
+    //         let x1 = ((vert2[0] +1.)*width as f32 *0.5) as i32;
+    //         let y1 = ((vert2[1] +1.)*height as f32 *0.5) as i32;
+
+    //         line(x0, y0, x1, y1, &mut imgbuf, white);
+    //     }
+    // }
 
     imgbuf = flip_vertical(&imgbuf);
 
-    imgbuf.save("res/head.png").unwrap();
+    imgbuf.save("res/test.png").unwrap();
 }
